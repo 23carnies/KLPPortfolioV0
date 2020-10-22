da83de3f7d815de28c1889becafcd999a68ebf16
23carnies <46233990+23carnies@users.noreply.github.com>
2020-10-22 10:52:22 -0400

    Button transition updates

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function() {
+    // https://dashboard.emailjs.com/admin/integration
+    emailjs.init('user_XufgNZrdHpOvCKiFai3ss');
+}();
+
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        this.contact_number.value = Math.random() * 100000 | 0;
+        emailjs.sendForm('contact_service', 'contact_form', this);
+        const form = document.getElementById('contact-form');
+        form.reset();
+        const messageEl = document.getElementById('message');
+        messageEl.innerText = 'Your message was successfully sent.'
+        setTimeout(() => {messageEl.className = 'hidden'}, 2000)
+    });
+}
